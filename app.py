9c65c9762f4d68379fb7a7f5b9138c28b197168a
derin-luck <aderinolasanyaolu@gmail.com>
2024-07-29 09:52:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ import re
 
 chatbot_response(user_inport):
 patterns = [
-    (r"hi|hello|hey","Hello! how can i help you today?")
+    (r"hi|hello|hey","Hello! how can i help you today?"),
+    (r"my name is (\w+)","nice to meet you ,{}!)
 ]
+
+for pattern,response in pattern:
+    match = re.search(pattern,user_inport,re.IGNORRECASE)
+    if match:
+        if callable(response):
+            return response(match)
